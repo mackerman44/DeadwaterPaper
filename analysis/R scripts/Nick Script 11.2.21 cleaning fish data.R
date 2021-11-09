6362016894822641c3ed081b6c722f96f46e3338
Nick Porter <60708163+port5518@users.noreply.github.com>
2021-11-09 11:43:33 -0700

    Added figures folder in the paper folder
    Added figures for the bioenergetics and a length frequency plot. There are 3 different plots one for a full year and no growth, one for a full year and NPM 10% growth, and one for no growth and only the 78 day fall season.

--- analysis/R scripts/Nick Script 11.2.21 cleaning fish data.R
+++ analysis/R scripts/Nick Script 11.2.21 cleaning fish data.R
@@ -3,8 +3,6 @@
 # 11/2/20201
 rm(list = ls()) # clear the working environment
 
-
-
 require(tidyverse)
 require(lubridate)
 require(boot)
@@ -19,8 +17,8 @@ require(car)
 require(dplyr)
 require(plotrix)
 require(nnet)
-require(Hmisc)
-require(Matching)
+#require(Hmisc) # does not work
+#require(Matching)
 require(Rcapture)
 require(nlstools)
 require(stringi)
@@ -39,33 +37,82 @@ Clean_NPM = NPM %>%
                                    stri_detect_fixed(StomachContents, "Empty") ~ "Empty", TRUE ~ "Other")) %>%
   drop_na(Length)
 
-#Read in teh Bioenergetics
+#Read in the Bioenergetics
 
 bioenergetics<- read_csv("analysis/data/raw data/2020 Bioenergetics 1 year.csv") %>%
   mutate(Date = as.POSIXct(Date, format = "%m/%d/%y")) %>%
-  mutate(Cumu_fish_eatten = cumsum(Cons_fish_g))
+  mutate(Cumu_fish_eatten = cumsum(Cons_fish_g)) %>%
+  mutate(Date = as.Date(Date)) %>%
+  mutate(month = month(Date))
+
+bioenergetics_10percet <- read_csv("analysis/data/raw data/2020 Bioenergetics 1 year 10% growth.csv") %>%
+mutate(Date = as.POSIXct(Date, format = "%m/%d/%y")) %>%
+  mutate(Cumu_fish_eatten = cumsum(Cons_fish_g)) %>%
+  mutate(Date = as.Date(Date)) %>%
+  mutate(month = month(Date))
+
 
 #Fall bioenergetics looking at 78 days
 Fall_bioenergetics = bioenergetics %>%
   filter(Date > "2020-08-31") %>%
   filter(Date < "2020-11-18") %>%
-  mutate(Cumu_fish_eatten = cumsum(Cons_fish_g))
+  mutate(Cumu_fish_eatten = cumsum(Cons_fish_g)) %>%
+  mutate(Date = as.Date(Date))
 
 #Plot of fish eatten
-Fall_bioenergetics %>%
-  filter(Date >= "2020-09-01 06:00:00" & Date <= "2020-11-17 07:00:00") %>%
+fall_bioenergetics = Fall_bioenergetics %>%
+  filter(Date >= "2020-09-01" & Date <= "2020-11-17") %>%
   ggplot() +
   aes(x = Date, y = Cumu_fish_eatten, colour = Temperature_C) +
   geom_line(size = 2.5) +
-  scale_color_viridis_c(option = "viridis", direction = 1) +
-  labs(y = "Cumulative fish eatten g") +
-  theme_classic()
+  scale_color_distiller(palette = "Spectral", direction = -1) +
+  #labs(y = "Cumulative fish eatten (g)") +
+  scale_x_date(breaks = "2 week") +
+  scale_y_continuous(name = "Cumulative fish eatten (g)", breaks = seq(0,200, by = 10),
+                     sec.axis = sec_axis(~. /12, name = "Potential number of Chinook smolts", breaks = seq(0,30, by = 1))) +
+  #theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1)) +
+  theme_classic() +
+  theme(legend.position = "top") +
+  labs(color = "Temperature (C)")
+
+fall_bioenergetics
+#Plot of fish eatten for 1 year and no growth for NPM
+Bioenergetics_plot <- bioenergetics %>%
+  ggplot() +
+  aes(x = Date, y = Cumu_fish_eatten, colour = Temperature_C) +
+  geom_line(size = 2.5) +
+  scale_color_distiller(palette = "Spectral", direction = -1) +
+  #labs(y = "Cumulative fish eatten (g)") +
+  #scale_x_date(breaks = "1 month") +
+  scale_y_continuous(name = "Cumulative fish eatten (g)", breaks = seq(0,600, by = 50),
+                     sec.axis = sec_axis(~. /12, name = "Potential number of Chinook smolts", breaks = seq(0,30, by = 2))) +
+  #theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1)) +
+  theme_classic() +
+  theme(legend.position = "top") +
+  labs(color = "Temperature (C)", x = "Month")
+
+Bioenergetics_plot
+
+#bioenergetics plot with 10% growth in NPM
+Bioenergetics_plot_10percent <- bioenergetics_10percet %>%
+  ggplot() +
+  aes(x = Date, y = Cumu_fish_eatten, colour = Temperature_C) +
+  geom_line(size = 2.5) +
+  scale_color_distiller(palette = "Spectral", direction = -1) +
+  #labs(y = "Cumulative fish eatten (g)") +
+  #scale_x_date(breaks = "1 month") +
+  scale_y_continuous(name = "Cumulative fish eatten (g)", breaks = seq(0,600, by = 50),
+                     sec.axis = sec_axis(~. /12, name = "Potential number of Chinook smolts", breaks = seq(0,40, by = 2))) +
+  #theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1)) +
+  theme_classic() +
+  theme(legend.position = "top") +
+  labs(color = "Temperature (C)", x = "Month")
+
+Bioenergetics_plot_10percent
 
 
 
-esquisse::esquisser(Fall_bioenergetics)
-
-
+#esquisse::esquisser(Fall_bioenergetics)
 
 #Histogram Ogle
 
@@ -81,18 +128,10 @@ Clean_NPM$NPM_50=lencat(Clean_NPM$Length, w = 50)
 #axis(side=1, at=xtick, labels = FALSE)####setting minor tick marks
 
 
-ggplot(data=Clean_NPM, aes(Length)) +
-  geom_histogram(breaks=seq(150, 650, by = 50),
-                 col="#DB740E",
-                 fill="#DB740E",
-                 alpha = .2) +
-  labs(x="TL (mm)", y="Frequency") +
-  xlim(c(100,700)) +
-  ylim(c(0,500)) +
-  theme_classic()
 
 
-Clean_NPM %>%
+
+Stomach_contents_per_sizeclass = Clean_NPM %>%
   filter(Date >= "2020-05-17 23:00:00" & Date <= "2020-11-21 00:00:00") %>%
   filter(Method %in%
            "Angling") %>%
@@ -103,3 +142,49 @@ Clean_NPM %>%
   theme_classic() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   facet_wrap(vars(NPM_50), nrow = 2L)
+
+Stomach_contents_per_sizeclass
+
+length_freq=ggplot(data=Clean_NPM, aes(Length)) +
+  geom_histogram(breaks=seq(150, 650, by = 50),
+                 col="#DB740E",
+                 fill="#DB740E",
+                 alpha = .2) +
+  labs(x="TL (mm)", y="Frequency") +
+  xlim(c(100,700)) +
+  ylim(c(0,800)) +
+  scale_x_continuous(breaks= seq(0,650, by = 50)) +
+  scale_y_continuous(breaks = seq(0,550, by = 50)) +
+  theme_classic()
+
+length_freq
+Stomach_contents_per_sizeclass
+Bioenergetics_plot
+Bioenergetics_plot_10percent
+fall_bioenergetics
+
+# Save fish passing plot
+ggsave('analysis/paper/figures/length_freq.jpg',
+       length_freq,
+       width = 5,
+       height = 4)
+
+ggsave('analysis/paper/figures/stomach content per sizeclass.jpg',
+       Stomach_contents_per_sizeclass,
+       width = 8,
+       height = 4)
+
+ggsave('analysis/paper/figures/bioenergetics.jpg',
+       Bioenergetics_plot,
+       width = 8,
+       height = 4)
+
+ggsave('analysis/paper/figures/bioenergetics 10 percent.jpg',
+       Bioenergetics_plot_10percent,
+       width = 8,
+       height = 4)
+
+ggsave('analysis/paper/figures/fall bioenergetics.jpg',
+       fall_bioenergetics,
+       width = 8,
+       height = 4)
